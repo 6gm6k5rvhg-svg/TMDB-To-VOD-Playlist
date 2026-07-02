d21f1d3ec331aa7f9fb79a708196570787407284
6gm6k5rvhg-svg <6gm6k5rvhg@privaterelay.appleid.com>
2026-07-02 13:19:02 -0700

    Add generate_live_playlist.php to Railway

--- generate_live_playlist.php
+++ generate_live_playlist.php
@@ -15,8 +15,7 @@ function runLivePlaylistGenerate() {
     $include = filter_var($INCLUDE_ADULT_VOD ?? false, FILTER_VALIDATE_BOOLEAN);
 
     $playlistUrl = $include
-        ? 'http://drewlive24.duckdns.org:8081/DrewLive/MergedPlaylist.m3u8'
-        : 'http://drewlive24.duckdns.org:8081/DrewLive/MergedCleanPlaylist.m3u8';
+        ? '$playlistUrl = 'https://raw.githubusercontent.com/gogetta69/public-files/main/Pluto-TV/us.m3u8';
 
     $categoriesFile = __DIR__ . "/channels/get_live_categories.json";
 
